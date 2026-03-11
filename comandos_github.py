e719e2f8a0a1c02e58ed2c490df2caf0976a7f2c
Tomiafk <milton10agustina@gmail.com>
2026-03-11 10:12:33 -0400

    Comandos

---
+++ comandos_github.py
@@ -0,0 +1,7 @@
+# Zona de comandos
+
+# git init  =  iniciar programa
+# git status  =  ver los status / sincronizacion
+# git add  =  añadir archivo al repositorio
+# git commit -m, "algo"  =  agregar al la nube de github con comentario
+# git push  =  actualizar en la nube
