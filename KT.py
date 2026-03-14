8df8f49259069fd55281bf0d83ed48c4d58d5e83
ailinsrosa88-maker <ailinsrosa88@gmail.com>
2026-03-14 14:02:15 -0400

    remove unnecessary files

--- KT.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from tkinter import *
-from tkinter import ttk
-root = Tk()
-frm = ttk.Frame(root, padding=100)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=90, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=50, row=0)
-root.mainloop()
