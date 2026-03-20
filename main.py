10e8ba52bfb7526bc1f183f284a92b4a9813f75a
Tomiafk <milton10agustina@gmail.com>
2026-03-20 14:58:44 -0400

    hola amiguitos

--- main.py
+++ main.py
@@ -3,10 +3,5 @@
 #import atm_ gui 
 #atm_gui.start() 
 from atm_gui import start_gui
-<<<<<<< HEAD
 
-start_gui()    
-
-=======
-start_gui()    
->>>>>>> e433ff24d38029315f20645d0200a5a6e5bc2b39
+start_gui()
